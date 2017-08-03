7c5de88f22c705dc9247fdca104374645294bf75
John Rey Tanquinco <johnreytanquinco@gmail.com>
2017-08-03 15:20:24 +0800

    Added drush installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "This is a sample script"
+echo "Installation starting..."
+
+echo "CD to Drupal directory"
+cd $DRUPAL_DIR
+	echo "Checking current directory..."
+	pwd
+    # drush -y site-install --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DRUPAL_SERVICE_NAME}-db/$MYSQL_DATABASE -r $DRUPAL_DIR
+    echo "Install Drupal"
+    drush -y site-install --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DRUPAL_SERVICE_NAME}-db/$MYSQL_DATABASE
+    echo "Drupal Installed"
+
