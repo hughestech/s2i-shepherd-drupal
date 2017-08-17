753d130f48012ec848c3ad33405580c8d70ab9a9
John Rey Tanquinco <johnreytanquinco@gmail.com>
2017-08-17 19:50:28 +0800

    Modifies ip, and added new file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ cd $DRUPAL_DIR
 	pwd
     # drush -y site-install --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DRUPAL_SERVICE_NAME}-db/$MYSQL_DATABASE -r $DRUPAL_DIR
     echo "Install Drupal"
-    drush -y site-install --db-url=mysql://userTY4:2Hi6AIPPRdfP0XLQ@10.128.1.152/drupal
+    drush -y site-install --db-url=mysql://userTY4:2Hi6AIPPRdfP0XLQ@10.128.1.126/drupal
     echo "Drupal Installed"
 
