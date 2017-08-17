827b2fbb80577d92333007a0c7d8191f9e0d7775
John Rey Tanquinco <johnreytanquinco@gmail.com>
2017-08-17 22:12:07 +0800

    End vars for drush install

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ cd $DRUPAL_DIR
 	pwd
     # drush -y site-install --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DRUPAL_SERVICE_NAME}-db/$MYSQL_DATABASE -r $DRUPAL_DIR
     echo "Install Drupal"
-    drush -y site-install --db-url=mysql://userTY4:2Hi6AIPPRdfP0XLQ@10.128.1.126/drupal
-    echo "Drupal Installed"
+    #drush -y site-install --db-url=mysql://userTY4:2Hi6AIPPRdfP0XLQ@10.128.1.126/drupal
+    echo "Running drush install..."
+	drush -y -v site-install --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DBHOST}/${MYSQL_DATABASE}
+	echo "OpenSocial site is successfully installed."
+	drush cr
 
