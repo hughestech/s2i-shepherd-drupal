3efd4dc800e790f6a26e78be117255de535872bb
John Rey Tanquinco <johnreytanquinco@gmail.com>
2017-08-24 00:05:36 +0800

    Tag version: 1.0.0-alpha1

--- automate.sh
+++ automate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Application Container
-appName=hughestech/opensocial:v1.3
+appName=hughestech/opensocial:1.0.0-alpha1
 
 # Creating builder image. Not sure if we need to pass env vars here. As long as they are added to dockerfile.
 docker build -t drupals2ibuilder . #-e MYSQL_USER=$MYSQL_USER -e MYSQL_PASSWORD=$MYSQL_PASSWORD -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
