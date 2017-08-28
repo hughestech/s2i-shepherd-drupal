0c5dc2fef4da43e2bf6d99de061bd9f0fb275640
John Rey Tanquinco <johnreytanquinco@gmail.com>
2017-08-28 12:30:18 +0800

    Added drupal checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,15 @@ DRUPAL_DIR=/var/www/html
 
 echo "CD to Drupal directory"
 cd $DRUPAL_DIR
-	echo "Checking current directory..."
-	pwd
-    # drush -y site-install --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DRUPAL_SERVICE_NAME}-db/$MYSQL_DATABASE -r $DRUPAL_DIR
-    echo "Install Drupal"
+echo "Checking current directory..."
+pwd
+if  drush status bootstrap | grep -q Successful
+then
+	echo "Install Drupal"
     #drush -y site-install --db-url=mysql://userTY4:2Hi6AIPPRdfP0XLQ@10.128.1.126/drupal
-    echo "Running drush install..."
 	drush -y -v site-install --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DBHOST}/${MYSQL_DATABASE}
 	echo "OpenSocial site is successfully installed."
 	drush cr
+else
+    echo "Installation failed!"
+fi
